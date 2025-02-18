ed15bff2014b89563813e027da8be7b0618cbc17
Gustavo Branquinho <gustavobranquinho2@gmail.com>
2025-02-18 19:33:48 -0300

    add user, branch, and driver entities with relationships; implement email validation utility

--- src/middlewares/handleError.ts
+++ src/middlewares/handleError.ts
@@ -1,14 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 import logger from "../config/winston";
 
-export const handleError = (
-  error: any,
-  request: Request,
-  response: Response,
-  next: NextFunction,
-) => {
+export const handleError = (error: any, _req: Request, res: Response, next: NextFunction) => {
   if (error?.statusCode === 500 || !error?.statusCode) {
     logger.error(error.message);
   }
-  response.status(error.statusCode || 500).json({ error: error.message });
+  res.status(error.statusCode || 500).json({ error: error.message });
 };
